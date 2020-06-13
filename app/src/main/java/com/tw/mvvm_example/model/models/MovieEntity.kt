167f8dca3e1eb8ddf1bb02a7aef9232d44b165ec
Gaby G <paogaby0803@gmail.com>
2020-06-13 00:26:15 -0500

    serializing data class movie

--- app/src/main/java/com/tw/mvvm_example/model/models/MovieEntity.kt
+++ app/src/main/java/com/tw/mvvm_example/model/models/MovieEntity.kt
@@ -1,4 +1,10 @@
 package com.tw.mvvm_example.model.models
+import kotlinx.serialization.Serializable
 
-class MovieEntity {
-}
+@Serializable
+data class MovieEntity constructor(
+    private val id: Int = 0,
+    private val title: String,
+    private val overview: String,
+    private val poster_path: String
+)
