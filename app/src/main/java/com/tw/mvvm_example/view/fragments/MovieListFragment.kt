9b38a59b2e5b03282f737f98d6936e9cfa24c2a4
PaolaCajilemaTW <tania.c@thoughtworks.com>
2020-06-14 19:53:07 -0500

    population of recyclerview

--- app/src/main/java/com/tw/mvvm_example/view/fragments/MovieListFragment.kt
+++ app/src/main/java/com/tw/mvvm_example/view/fragments/MovieListFragment.kt
@@ -1,4 +1,13 @@
 package com.tw.mvvm_example.view.fragments
 
-class MovieListFragment {
+import androidx.fragment.app.Fragment
+import com.tw.mvvm_example.transactionalmodels.Movie
+import com.tw.mvvm_example.view.adapters.MovieAdapter
+import kotlinx.android.synthetic.main.layout_movies.*
+
+class MovieListFragment : Fragment() {
+
+    private fun populateMovies(movies: List<Movie>) {
+        rv_movies.adapter = MovieAdapter(movies)
+    }
 }
