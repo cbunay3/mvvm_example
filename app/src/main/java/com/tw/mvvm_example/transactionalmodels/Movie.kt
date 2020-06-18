a3796bdd3226d21cd249c445ffe256e81fa989e1
Gaby G <paogaby0803@gmail.com>
2020-06-18 12:44:44 -0500

    consume API movies

--- app/src/main/java/com/tw/mvvm_example/transactionalmodels/Movie.kt
+++ app/src/main/java/com/tw/mvvm_example/transactionalmodels/Movie.kt
@@ -4,8 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Movie constructor(
-    private val id: Int = 0,
-    private val title: String,
+    private val id: Int,
+    private val poster_path: String,
     private val overview: String,
-    private val poster_path: String
+    private val release_date: String,
+    private val title: String,
+    private val original_title: String,
+    private val original_language: String
 ) : java.io.Serializable
