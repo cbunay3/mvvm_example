8e77754c73371edf5b417179417bbb586f8c8c53
jrodri0790 <jrodri@thoughtworks.com>
2020-06-18 19:01:50 -0500

    consume API movies (#1)
    * consume API movies
    
    * consume API movies
    
    Co-authored-by: Gaby G <paogaby0803@gmail.com>

--- app/src/main/java/com/tw/mvvm_example/transactionalmodels/MoviesWrapper.kt
+++ app/src/main/java/com/tw/mvvm_example/transactionalmodels/MoviesWrapper.kt
@@ -1,8 +1,6 @@
 package com.tw.mvvm_example.transactionalmodels
 
 import kotlinx.serialization.Serializable
-import com.tw.mvvm_example.transactionalmodels.Movie
 
 @Serializable
-data class MoviesWrapper(val movies: List<Movie>) :
-    java.io.Serializable
+data class MoviesWrapper(val results: List<Movie>):java.io.Serializable
--- app/src/main/java/com/tw/mvvm_example/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tw/mvvm_example/viewmodel/MainViewModel.kt
@@ -1,11 +1,18 @@
 package com.tw.mvvm_example.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.tw.mvvm_example.model.services.MovieService
+import com.tw.mvvm_example.transactionalmodels.Movie
+import com.tw.mvvm_example.transactionalmodels.MoviesWrapper
+import retrofit2.Call
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val movieService: MovieService
+) : ViewModel() {
+    var movies: ArrayList<Movie> = arrayListOf()
 
-    fun getWelcomeMessage(): String{
-        return "Hello World!"
+    fun  getMoviesService(): Call<MoviesWrapper> {
+        return movieService.getMoviesService()
     }
 
 }
