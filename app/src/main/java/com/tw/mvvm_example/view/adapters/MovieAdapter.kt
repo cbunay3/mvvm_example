f3530c71adf1eec74d12e782584fbde4614beb2f
PaolaCajilemaTW <55449647+PaolaCajilemaTW@users.noreply.github.com>
2020-06-23 09:42:27 -0500

    showing-list-of-movies (#2)
    * Added cardView to show one movie
    
    * loading movie data in CardView
    
    * Added a recyclerView
    
    * population of recyclerview
    
    * Added more elements
    
    * Add dependencies in gradle for recyclerView and CardView
    
    * Add navigation with your dependencies
    
    * Add navigation to list movies. Add style to the item
    
    * Add style to the cardView
    
    * delete scrollView
    
    * change style of cardView

--- app/src/main/java/com/tw/mvvm_example/view/adapters/MovieAdapter.kt
+++ app/src/main/java/com/tw/mvvm_example/view/adapters/MovieAdapter.kt
@@ -1,4 +1,40 @@
 package com.tw.mvvm_example.view.adapters
 
-class MovieAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tw.mvvm_example.R
+import com.tw.mvvm_example.transactionalmodels.Movie
+import kotlinx.android.synthetic.main.layout_movie.view.*
+
+class MovieAdapter(
+    private val items: List<Movie>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) =
+            with(itemView) {
+                tv_title.text = movie.title
+                tv_originalTitle.text = movie.original_title
+                tv_releaseDate.text = movie.release_date
+                itemView.setOnClickListener {
+                    println("redirecting to the details")
+                }
+            }
+    }
 }
