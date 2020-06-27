a6255f5b38aa35e11ce5136f3a54ea0466b86203
Carlos Bunay <carlos.bunay@thoughtworks.com>
2020-06-27 13:42:06 -0500

    Add Entity, DAO and DTO clases for movies

--- app/src/main/java/com/tw/mvvm_example/model/daos/MovieDao.kt
+++ app/src/main/java/com/tw/mvvm_example/model/daos/MovieDao.kt
@@ -1,4 +1,17 @@
 package com.tw.mvvm_example.model.daos
 
-class MovieDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.tw.mvvm_example.model.models.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Transaction
+    fun insert(obj: List<MovieEntity>)
+
+    @Query("SELECT * FROM MovieEntity ORDER BY id DESC")
+    fun getAll(): LiveData<List<MovieEntity>>
+
 }
