dc80daed9e0618b30732beae5b380e4c62420fe4
Carlos Bunay <carlos.bunay@thoughtworks.com>
2020-06-27 13:44:41 -0500

    Add repository and mapper for movies

--- app/src/main/java/com/tw/mvvm_example/model/mappers/MovieMapper.kt
+++ app/src/main/java/com/tw/mvvm_example/model/mappers/MovieMapper.kt
@@ -1,4 +1,40 @@
 package com.tw.mvvm_example.model.mappers
 
+import com.tw.mvvm_example.model.dtos.MovieDto
+import com.tw.mvvm_example.model.models.MovieEntity
+
 class MovieMapper {
+
+    fun mapToEntity(moviesDto: List<MovieDto>): List<MovieEntity> {
+        val moviesEntity: MutableList<MovieEntity> = mutableListOf()
+        moviesDto.map { moviesEntity.add(mapToEntity(it)) }
+        return moviesEntity
+    }
+
+    fun mapToEntity(movieDto: MovieDto) = MovieEntity(
+        id = movieDto.id,
+        title = movieDto.title,
+        overview = movieDto.overview,
+        poster_path = movieDto.poster_path,
+        release_date = movieDto.release_date,
+        original_title = movieDto.original_title,
+        original_language = movieDto.original_language
+    )
+
+    fun mapToDto(moviesEntity: List<MovieEntity>): List<MovieDto> {
+        val moviesDto: MutableList<MovieDto> = mutableListOf()
+        moviesEntity.map { moviesDto.add(mapToDto(it)) }
+        return moviesDto
+    }
+
+    fun mapToDto(movieEntity: MovieEntity) = MovieDto(
+        id = movieEntity.id,
+        title = movieEntity.title,
+        overview = movieEntity.overview,
+        poster_path = movieEntity.poster_path,
+        release_date = movieEntity.release_date,
+        original_title = movieEntity.original_title,
+        original_language = movieEntity.original_language
+    )
+
 }
--- app/src/main/java/com/tw/mvvm_example/model/repositories/MovieRepository.kt
+++ app/src/main/java/com/tw/mvvm_example/model/repositories/MovieRepository.kt
@@ -1,4 +1,23 @@
 package com.tw.mvvm_example.model.repositories
 
-class MovieRepository {
+import androidx.lifecycle.LiveData
+import com.tw.mvvm_example.database.ApplicationDatabase
+import com.tw.mvvm_example.model.daos.MovieDao
+import com.tw.mvvm_example.model.dtos.MovieDto
+import com.tw.mvvm_example.model.mappers.MovieMapper
+import com.tw.mvvm_example.model.models.MovieEntity
+
+class MovieRepository(dataBase: ApplicationDatabase, private val movieMapper: MovieMapper) {
+
+    private val movieDao: MovieDao = dataBase.movieDao()
+
+    fun save(moviesDto: List<MovieDto>): List<MovieEntity> {
+        var moviesEntity = movieMapper.mapToEntity(moviesDto)
+        movieDao.insert(moviesEntity)
+        return moviesEntity
+    }
+
+    fun getAll(): LiveData<List<MovieEntity>> {
+        return movieDao.getAll()
+    }
 }
