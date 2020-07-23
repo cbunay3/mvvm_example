76b6d7bd0839704bc131912e7e8c071787d34b96
PaolaCajilemaTW <tania.c@thoughtworks.com>
2020-07-22 21:24:19 -0500

    handling null values

--- app/src/main/java/com/tw/mvvm_example/model/models/MovieEntity.kt
+++ app/src/main/java/com/tw/mvvm_example/model/models/MovieEntity.kt
@@ -6,10 +6,10 @@ import androidx.room.PrimaryKey
 @Entity
 data class MovieEntity constructor(
     @PrimaryKey var id: Int,
-    var title: String,
-    var overview: String,
-    var posterPath: String,
-    var releaseDate: String,
-    var originalTitle: String,
-    var originalLanguage: String
+    var title: String?,
+    var overview: String?,
+    var posterPath: String?,
+    var releaseDate: String?,
+    var originalTitle: String?,
+    var originalLanguage: String?
 )
